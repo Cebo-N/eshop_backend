8856bcb08ef5e92a27495ebc7a326b52fc56ea4e
Cebo N <cebonqweniso00@gmail.com>
2021-12-13 21:00:16 +0200

    exclude api paths that do not need authentication

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -2,10 +2,26 @@ const expressJwt = require('express-jwt');
 
 const authentication = () =>{
     const secret = process.env.SECRET_KEY
+    const api = process.env.API_URL
     return expressJwt({
         secret,
-        algorithms : ['HS256']
+        algorithms : ['HS256'],
+        isRevoked : isRevoked
+    }).unless({
+        path : [
+            {url : /\/api\/v1\/products(.*)/, method : ['GET','OPTIONS']},
+            {url : /\/api\/v1\/categories(.*)/, method : ['GET','OPTIONS']},
+            `${api}/users/login`,
+            `${api}/users/register`
+        ]
     })
+}
+
+const isRevoked = async(req, payload, done) =>{
+    if(!payload.isAdmin){
+        done(null, true)
+    }
+    done();
 }
 
 module.exports = authentication
